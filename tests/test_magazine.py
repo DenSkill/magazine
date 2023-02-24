1541dc4f2f52641348fbe29ad8b97bd018d47adf
jkfknbchdx <denskillhigh@mail.ru>
2023-02-24 23:27:17 +0500

    wtf with tests?((

--- tests/test_magazine.py
+++ tests/test_magazine.py
@@ -1,10 +1,12 @@
 import pytest
-from main import Item
+from magazine import Item
 
-item3 = Item("Смартфон", 10000, 20)
 
-def test_apply_discount(item3):
+
+def test_apply_discount():
+    item3 = Item("Смартфон", 10000, 20)
     assert item3.apply_discount() == 8000.0
 
 def test_calculate_total_price():
+    item3 = Item("Смартфон", 10000, 20)
     assert item3.calculate_total_price(1000, 20) == 20000
