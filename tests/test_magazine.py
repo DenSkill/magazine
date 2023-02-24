1d44a4597b551ca73e4e09a12e4b8032f1df88f9
jkfknbchdx <denskillhigh@mail.ru>
2023-02-25 00:40:46 +0500

    POETRY TEST!!

--- tests/test_magazine.py
+++ tests/test_magazine.py
@@ -1,12 +1,6 @@
+from main.magazine import Item
 import pytest
-from magazine import Item
-
-
-
-def test_apply_discount():
-    item3 = Item("Смартфон", 10000, 20)
-    assert item3.apply_discount() == 8000.0
 
 def test_calculate_total_price():
     item3 = Item("Смартфон", 10000, 20)
-    assert item3.calculate_total_price(1000, 20) == 20000
+    assert item3.calculate_total_price() == 200000
