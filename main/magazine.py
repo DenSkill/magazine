e90e250de8d9da2af932ef829e76668e9d45c1a6
jkfknbchdx <denskillhigh@mail.ru>
2023-02-26 20:45:32 +0500

    2hw + с кривые тесты

--- main/magazine.py
+++ main/magazine.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     pay_rate = 1.0
     all = []
@@ -14,13 +17,41 @@ class Item:
     def calculate_total_price(self):
         return (self.price * self.count)
 
+    @staticmethod
+    def is_int(data) -> bool:
+        if float(data).is_integer():
+            return True
+        return False
 
-item1 = Item("Смартфон", 10000, 20)
-item2 = Item("Ноутбук", 20000, 5)
-print(item1.calculate_total_price())
-print(item2.calculate_total_price())
-Item.pay_rate = 0.8  # устанавливаем новый уровень цен
-item1.apply_discount()
-print(item1.price)
-print(item2.price)
-print(Item.all)
+    @classmethod
+    def new_copy(cls) -> 'Item':
+        """Создаёт новые экзэмпляры из csv файла"""
+        copies = []
+        with open('../items.csv', 'r', encoding="UTF-8", newline='') as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=',')
+            for row in reader:
+                cls(row['name'], int(row['price']), int(row['quantity']))
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value: str):
+        if len(value) <= 10:
+            self.__name = value
+        else:
+            print('Exception: Длина наименования товара превышает 10 символов.')
+
+    def get_price(self):
+        """Возвращает цену товра"""
+        return self.price * self.count
+
+
+Item.new_copy()  # создание объектов из данных файла
+print(len(Item.all))  # в файле 5 записей
+item1 = Item.all[0]
+print(item1.name)
+print(Item.is_int(5))
+print(Item.is_int(5.0))
+print(Item.is_int(5.5))
--- tests/test_magazine.py
+++ tests/test_magazine.py
@@ -1,6 +1,17 @@
 from main.magazine import Item
+import csv
 import pytest
 
 def test_calculate_total_price():
     item3 = Item("Смартфон", 10000, 20)
     assert item3.calculate_total_price() == 200000
+
+def test_name_length_10(): # тест длины названия
+    item = Item('Смартфон',10000,20)
+    item2 = Item('СССССССмартфон',10000,20)
+    assert len(item.name) <= 10
+    assert len(item2.name) > 10
+
+def test_is_int():
+    assert Item.is_int(5) == True
+    assert Item.is_int(5.5) == False
